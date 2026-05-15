c6078ac7593e01627294e76675162a0183a3260c
XiaoColorful <120188147+XColorful@users.noreply.github.com>
2026-05-15 14:22:52 +0800

    Port resource-skeleton to 1.21.4neoforge

--- neoforge-compat/src/main/java/xiao/customgun/neoforgeclient/event/events/NeoAddClientReloadListenerEventManager.java
+++ neoforge-compat/src/main/java/xiao/customgun/neoforgeclient/event/events/NeoAddClientReloadListenerEventManager.java
@@ -2,7 +2,7 @@ package xiao.customgun.neoforgeclient.event.events;
 
 import net.neoforged.bus.api.Event;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.neoforge.client.event.RegisterClientReloadListenersEvent;
+import net.neoforged.neoforge.client.event.AddClientReloadListenersEvent;
 import xiao.customgun.core.api.event.EventPriority;
 import xiao.customgun.core.api.event.EventType;
 import xiao.customgun.core.api.event.IEventHandler;
@@ -51,7 +51,7 @@ public class NeoAddClientReloadListenerEventManager {
             return new NeoAddClientReloadListenerEvent(event);
         }
 
-        protected void handle(RegisterClientReloadListenersEvent event) {
+        protected void handle(AddClientReloadListenersEvent event) {
             super.onEvent(event);
         }
     }
@@ -59,30 +59,30 @@ public class NeoAddClientReloadListenerEventManager {
     public static class AddClientReloadListenerProxyHighest extends AddClientReloadListenerProxy {
         static final AddClientReloadListenerProxyHighest INSTANCE = new AddClientReloadListenerProxyHighest();
         @SubscribeEvent(priority = net.neoforged.bus.api.EventPriority.HIGHEST, receiveCanceled = true)
-        public void onEvent(RegisterClientReloadListenersEvent e) { handle(e); }
+        public void onEvent(AddClientReloadListenersEvent e) { handle(e); }
     }
 
     public static class AddClientReloadListenerProxyHigh extends AddClientReloadListenerProxy {
         static final AddClientReloadListenerProxyHigh INSTANCE = new AddClientReloadListenerProxyHigh();
         @SubscribeEvent(priority = net.neoforged.bus.api.EventPriority.HIGH, receiveCanceled = true)
-        public void onEvent(RegisterClientReloadListenersEvent e) { handle(e); }
+        public void onEvent(AddClientReloadListenersEvent e) { handle(e); }
     }
 
     public static class AddClientReloadListenerProxyNormal extends AddClientReloadListenerProxy {
         static final AddClientReloadListenerProxyNormal INSTANCE = new AddClientReloadListenerProxyNormal();
         @SubscribeEvent(priority = net.neoforged.bus.api.EventPriority.NORMAL, receiveCanceled = true)
-        public void onEvent(RegisterClientReloadListenersEvent e) { handle(e); }
+        public void onEvent(AddClientReloadListenersEvent e) { handle(e); }
     }
 
     public static class AddClientReloadListenerProxyLow extends AddClientReloadListenerProxy {
         static final AddClientReloadListenerProxyLow INSTANCE = new AddClientReloadListenerProxyLow();
         @SubscribeEvent(priority = net.neoforged.bus.api.EventPriority.LOW, receiveCanceled = true)
-        public void onEvent(RegisterClientReloadListenersEvent e) { handle(e); }
+        public void onEvent(AddClientReloadListenersEvent e) { handle(e); }
     }
 
     public static class AddClientReloadListenerProxyLowest extends AddClientReloadListenerProxy {
         static final AddClientReloadListenerProxyLowest INSTANCE = new AddClientReloadListenerProxyLowest();
         @SubscribeEvent(priority = net.neoforged.bus.api.EventPriority.LOWEST, receiveCanceled = true)
-        public void onEvent(RegisterClientReloadListenersEvent e) { handle(e); }
+        public void onEvent(AddClientReloadListenersEvent e) { handle(e); }
     }
 }

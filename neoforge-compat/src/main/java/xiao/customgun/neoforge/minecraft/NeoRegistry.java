70e61f07f01ab06c9d11ca22cfec758bdc69aed8
XiaoColorful <120188147+XColorful@users.noreply.github.com>
2026-05-18 00:49:18 +0800

    Port #1 to neoforge1.20.2

--- neoforge-compat/src/main/java/xiao/customgun/neoforge/minecraft/NeoRegistry.java
+++ neoforge-compat/src/main/java/xiao/customgun/neoforge/minecraft/NeoRegistry.java
@@ -23,7 +23,7 @@ import xiao.customgun.core.api.minecraft.IMcRegistry;
 public class NeoRegistry implements IMcRegistry {
 
     @Override public ResourceLocation createResourceLocation(String rlString) {
-        return new ResourceLocation(rlString);
+        return ResourceLocation.tryParse(rlString);
     }
 
     @Override public @Nullable Block getBlock(ResourceLocation rl) {

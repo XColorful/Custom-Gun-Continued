7352200acab0f1e1ac708aa225f8fc98b84f8941
XiaoColorful <120188147+XColorful@users.noreply.github.com>
2026-05-31 16:28:17 +0800

    Port #7 to neoforge1.20.2

--- neoforge-compat/src/main/java/xiao/customgun/neoforge/CustomGunNeoforge.java
+++ neoforge-compat/src/main/java/xiao/customgun/neoforge/CustomGunNeoforge.java
@@ -76,7 +76,9 @@ public class CustomGunNeoforge {
         NeoCapabilityRegistry.onRegisterCapabilities(modEventBus);
 
         ModRecipe.RECIPE_SERIALIZERS.registerAll(modEventBus);
+        ModRecipe.RECIPE_SERIALIZERS_OLD1.registerAll(modEventBus);
         ModRecipe.RECIPE_TYPES.registerAll(modEventBus);
+        ModRecipe.RECIPE_TYPES_OLD1.registerAll(modEventBus);
         ModSounds.SOUNDS.registerAll(modEventBus);
 
         if (mcSide == McSide.CLIENT) {

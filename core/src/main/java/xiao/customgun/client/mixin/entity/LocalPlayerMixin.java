f6811fec8e75597bd2a58498c7f8c03926ffcc5b
XiaoColorful <XColor_ful@outlook.com>
2026-06-05 14:58:09 +0800

    prefix LocalPlayer mixin fields

--- core/src/main/java/xiao/customgun/client/mixin/entity/LocalPlayerMixin.java
+++ core/src/main/java/xiao/customgun/client/mixin/entity/LocalPlayerMixin.java
@@ -22,41 +22,41 @@ import xiao.customgun.core.api.entity.ShootResult;
 
 @Mixin(LocalPlayer.class)
 public class LocalPlayerMixin implements ILocalShooter {
-    private final LocalPlayer cgc$localPlayer = (LocalPlayer) (Object) this;
-    private final LocalShooterProperty cgc$localShooterProperty = new LocalShooterProperty(cgc$localPlayer);
+    private final LocalPlayer cgc$localShooter = (LocalPlayer) (Object) this;
+    private final LocalShooterProperty cgc$localShooterProperty = new LocalShooterProperty(cgc$localShooter);
 
     // 行为动作
-    private final LocalShooterCrawl cgc$crawl = new LocalShooterCrawl(cgc$localPlayer, cgc$localShooterProperty);
-    private final LocalShooterSprint cgc$sprint = new LocalShooterSprint(cgc$localPlayer, cgc$localShooterProperty);
+    private final LocalShooterCrawl cgc$localCrawl = new LocalShooterCrawl(cgc$localShooter, cgc$localShooterProperty);
+    private final LocalShooterSprint cgc$localSprint = new LocalShooterSprint(cgc$localShooter, cgc$localShooterProperty);
 
     // 枪械操作
-    private final LocalShooterDraw cgc$draw = new LocalShooterDraw(cgc$localPlayer, cgc$localShooterProperty);
-    private final LocalShooterFireSelect cgc$fireSelect = new LocalShooterFireSelect(cgc$localPlayer, cgc$localShooterProperty);
-    private final LocalShooterAim cgc$aim = new LocalShooterAim(cgc$localPlayer, cgc$localShooterProperty);
-    private final LocalShooterMelee cgc$melee = new LocalShooterMelee(cgc$localPlayer, cgc$localShooterProperty);
-    private final LocalShooterShoot cgc$shoot = new LocalShooterShoot(cgc$localPlayer, cgc$localShooterProperty);
-    private final LocalShooterBolt cgc$bolt = new LocalShooterBolt(cgc$localPlayer, cgc$localShooterProperty);
-    private final LocalShooterReload cgc$reload = new LocalShooterReload(cgc$localPlayer, cgc$localShooterProperty);
+    private final LocalShooterDraw cgc$localDraw = new LocalShooterDraw(cgc$localShooter, cgc$localShooterProperty);
+    private final LocalShooterFireSelect cgc$localFireSelect = new LocalShooterFireSelect(cgc$localShooter, cgc$localShooterProperty);
+    private final LocalShooterAim cgc$localAim = new LocalShooterAim(cgc$localShooter, cgc$localShooterProperty);
+    private final LocalShooterMelee cgc$localMelee = new LocalShooterMelee(cgc$localShooter, cgc$localShooterProperty);
+    private final LocalShooterShoot cgc$localShoot = new LocalShooterShoot(cgc$localShooter, cgc$localShooterProperty);
+    private final LocalShooterBolt cgc$localBolt = new LocalShooterBolt(cgc$localShooter, cgc$localShooterProperty);
+    private final LocalShooterReload cgc$localReload = new LocalShooterReload(cgc$localShooter, cgc$localShooterProperty);
 
-    private final LocalShooterInspect cgc$inspect = new LocalShooterInspect(cgc$localPlayer, cgc$localShooterProperty);
+    private final LocalShooterInspect cgc$localInspect = new LocalShooterInspect(cgc$localShooter, cgc$localShooterProperty);
 
     @Inject(method = "tick", at = @At("HEAD"))
     public void onLocalTick(CallbackInfo ci) {
-        if (cgc$localPlayer.level().isClientSide()) {
-            this.cgc$aim.tickAimingProgress();
-            this.cgc$crawl.tickCrawl();
+        if (cgc$localShooter.level().isClientSide()) {
+            this.cgc$localAim.tickAimingProgress();
+            this.cgc$localCrawl.tickCrawl();
             this.cgc$localShooterProperty.tickStateLock();
-            this.cgc$bolt.tickAutoBolt();
-            cgc$localPlayer.setSprinting(this.cgc$sprint.getProcessedSprintStatus(cgc$localPlayer.isSprinting()));
+            this.cgc$localBolt.tickAutoBolt();
+            cgc$localShooter.setSprinting(this.cgc$localSprint.getProcessedSprintStatus(cgc$localShooter.isSprinting()));
         }
     }
 
     @WrapOperation(method = "aiStep", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/player/LocalPlayer;setSprinting(Z)V"))
     public void swapSprintStatus(LocalPlayer player, boolean sprinting, Operation<Void> original) {
         if (sprinting) {
-            this.cgc$reload.cancelReload();
+            this.cgc$localReload.cancelReload();
         }
-        original.call(player, this.cgc$sprint.getProcessedSprintStatus(sprinting));
+        original.call(player, this.cgc$localSprint.getProcessedSprintStatus(sprinting));
     }
 
     @Inject(method = "respawn", at = @At("RETURN"))
@@ -77,74 +77,74 @@ public class LocalPlayerMixin implements ILocalShooter {
 
     @Override
     public void cgc$crawl(boolean isCrawl) {
-        this.cgc$crawl.crawl(isCrawl);
+        this.cgc$localCrawl.crawl(isCrawl);
     }
 
     // --------枪械操作--------
 
     @Override public void cgc$clientDraw(ItemStack lastItem) {
-        this.cgc$draw.draw(lastItem);
+        this.cgc$localDraw.draw(lastItem);
     }
     @Override public void cgc$resetDraw() {
-        this.cgc$draw.setReadyToDraw(false);
+        this.cgc$localDraw.setReadyToDraw(false);
     }
 
     @Override
     public void cgc$fireSelect() {
-        this.cgc$fireSelect.fireSelect();
+        this.cgc$localFireSelect.fireSelect();
     }
 
     @Override
     public void cgc$aim(boolean isAim) {
-        this.cgc$aim.aim(isAim);
+        this.cgc$localAim.aim(isAim);
     }
 
     @Override
     public void cgc$melee() {
-        this.cgc$melee.melee();
+        this.cgc$localMelee.melee();
     }
 
     @Override public ShootResult cgc$localShoot() {
-        this.cgc$reload.cancelReload();
-        return this.cgc$shoot.shoot();
+        this.cgc$localReload.cancelReload();
+        return this.cgc$localShoot.shoot();
     }
     @Override public boolean cgc$chargeShoot(boolean isCharge) {
-        return this.cgc$shoot.chargeShoot(isCharge);
+        return this.cgc$localShoot.chargeShoot(isCharge);
     }
 
     @Override
     public void cgc$bolt() {
-        this.cgc$bolt.bolt();
+        this.cgc$localBolt.bolt();
     }
 
     @Override
     public void cgc$reload() {
-        this.cgc$reload.reload();
+        this.cgc$localReload.reload();
     }
 
     @Override public void cgc$inspect() {
-        this.cgc$inspect.inspect();
+        this.cgc$localInspect.inspect();
     }
 
     // --------ILocalShooterState--------
 
     @Override public boolean cgc$isAim() {
-        return this.cgc$aim.isAim();
+        return this.cgc$localAim.isAim();
     }
     @Override public boolean cgc$isCrawl() {
-        return this.cgc$crawl.isCrawling();
+        return this.cgc$localCrawl.isCrawling();
     }
     @Override public boolean cgc$isReadyToDraw() {
-        return this.cgc$draw.isReadyToDraw();
+        return this.cgc$localDraw.isReadyToDraw();
     }
     @Override public boolean cgc$isCharging() {
         return this.cgc$localShooterProperty.isCharging;
     }
     @Override public float cgc$getAimingProgress(float partialTicks) {
-        return this.cgc$aim.getClientAimingProgress(partialTicks);
+        return this.cgc$localAim.getClientAimingProgress(partialTicks);
     }
     @Override public long cgc$getShootCooldown() {
-        return this.cgc$shoot.getClientShootCooldown();
+        return this.cgc$localShoot.getClientShootCooldown();
     }
     @Override public float cgc$getChargeProgress() {
         return this.cgc$localShooterProperty.chargeProgress;

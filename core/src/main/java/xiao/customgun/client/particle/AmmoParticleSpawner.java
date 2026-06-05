761da08b73267c07a505f9ac5eebc21cfbbfccf8
XiaoColorful <XColor_ful@outlook.com>
2026-06-06 01:33:14 +0800

    Particle skeleton

---
+++ core/src/main/java/xiao/customgun/client/particle/AmmoParticleSpawner.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2024-2026 MCModderAnchor (https://github.com/MCModderAnchor)
+ * SPDX-License-Identifier: GPL-3.0-only
+ *
+ * Source: https://github.com/MCModderAnchor/TACZ
+ */
+
+package xiao.customgun.client.particle;
+
+import xiao.customgun.core.developer.PlannedRefactor;
+
+public class AmmoParticleSpawner {
+
+    public static void spawnParticle() {
+        if (PlannedRefactor.ON_SPAWN_BULLET_PARTICLE) return;
+        // TODO
+    }
+}

88604b9758655ac8932127642051d3354f584971
XiaoColorful <120188147+XColorful@users.noreply.github.com>
2026-06-06 15:03:21 +0800

    Port #15 to neoforge1.21.1

--- neoforge-compat/src/main/java/xiao/customgun/neoforgeclient/init/NeoClientModParticles.java
+++ neoforge-compat/src/main/java/xiao/customgun/neoforgeclient/init/NeoClientModParticles.java
@@ -6,12 +6,12 @@ package xiao.customgun.neoforgeclient.init;
 
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.RegisterParticleProvidersEvent;
 import xiao.customgun.CustomGun;
 import xiao.customgun.client.init.ClientModParticles;
 
-@Mod.EventBusSubscriber(value = Dist.CLIENT, modid = CustomGun.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+@EventBusSubscriber(value = Dist.CLIENT, modid = CustomGun.MOD_ID)
 public class NeoClientModParticles {
 
     public static ClientModParticles CLIENT_MOD_PARTICLES = ClientModParticles.get();

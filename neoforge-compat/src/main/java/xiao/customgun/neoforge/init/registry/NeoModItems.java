97437e6ee2144b5af750fc32cab02b074d6acc02
XiaoColorful <120188147+XColorful@users.noreply.github.com>
2026-06-08 14:43:18 +0800

    Port #16 to 1.21.1neoforge

--- neoforge-compat/src/main/java/xiao/customgun/neoforge/init/registry/NeoModItems.java
+++ neoforge-compat/src/main/java/xiao/customgun/neoforge/init/registry/NeoModItems.java
@@ -1,6 +1,12 @@
 package xiao.customgun.neoforge.init.registry;
 
 import net.minecraft.world.item.Item;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.extensions.common.RegisterClientExtensionsEvent;
+import org.jetbrains.annotations.ApiStatus;
+import xiao.customgun.CustomGun;
 import xiao.customgun.core.init.registry.ModItems;
 import xiao.customgun.core.item.ammo.AmmoItem;
 import xiao.customgun.core.item.attachment.AttachmentItem;
@@ -16,6 +22,7 @@ import java.util.function.Supplier;
 /**
  * 处理需要hack的{@link ModItems}注册类型
  */
+@EventBusSubscriber(value = Dist.CLIENT, modid = CustomGun.MOD_ID)
 public class NeoModItems {
 
     private static final Map<Class<? extends Item>, Supplier<? extends Item>> NEO_ITEM_MAP = new HashMap<>();
@@ -35,5 +42,23 @@ public class NeoModItems {
         }
 
         throw new IllegalArgumentException("No NeoForge item mapping registered for core class: " + clazz.getName());
+    }
+
+    // --------1.21.1--------
+    /**
+     * 手动添加{@link #NEO_ITEM_MAP}新增的{@link #_put}
+     */
+    @ApiStatus.AvailableSince("1.21.1")
+    @SubscribeEvent
+    public static void registerClientExtensions(RegisterClientExtensionsEvent event) {
+        if (NEO_ITEM_MAP.size() != 3) {
+            throw new IllegalStateException("NEO_ITEM_MAP size mismatch! Extensions registration is missing.");
+        }
+        if (!(ModItems.GUN.get() instanceof NeoGunItem neoGunItem)) throw new IllegalArgumentException("ModItems.GUN.get() is not NeoGunItem!");
+        NeoGunItem.registerClientExtension(extensions -> event.registerItem(extensions, neoGunItem));
+        if (!(ModItems.ATTACHMENT.get() instanceof NeoAttachmentItem neoAttachmentItem)) throw new IllegalArgumentException("ModItems.ATTACHMENT.get() is not NeoAttachmentItem!");
+        NeoAttachmentItem.registerClientExtension(extensions -> event.registerItem(extensions, neoAttachmentItem));
+        if (!(ModItems.AMMO.get() instanceof NeoAmmoItem neoAmmoItem)) throw new IllegalArgumentException("ModItems.AMMO.get() is not NeoAmmoItem!");
+        NeoAmmoItem.registerClientExtension(extensions -> event.registerItem(extensions, neoAmmoItem));
     }
 }
--- neoforge-compat/src/main/java/xiao/customgun/neoforge/item/ammo/NeoAmmoItem.java
+++ neoforge-compat/src/main/java/xiao/customgun/neoforge/item/ammo/NeoAmmoItem.java
@@ -17,12 +17,6 @@ import java.util.function.Consumer;
 public class NeoAmmoItem extends AmmoItem {
 
     @OnlyIn(Dist.CLIENT)
-    @Override
-    public void initializeClient(Consumer<IClientItemExtensions> consumer) {
-        registerClientExtension(consumer);
-    }
-
-    @OnlyIn(Dist.CLIENT)
     public static void registerClientExtension(Consumer<IClientItemExtensions> consumer) {
     }
 }

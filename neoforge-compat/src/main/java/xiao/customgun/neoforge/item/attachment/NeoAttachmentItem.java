8826f2bebab59212100eecbc5c7c768d4329d293
XiaoColorful <120188147+XColorful@users.noreply.github.com>
2026-06-08 11:38:09 +0800

    Port #16 to neoforge1.20.2

---
+++ neoforge-compat/src/main/java/xiao/customgun/neoforge/item/attachment/NeoAttachmentItem.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2024-2026 MCModderAnchor (https://github.com/MCModderAnchor)
+ * SPDX-License-Identifier: GPL-3.0-only
+ *
+ * Source: https://github.com/MCModderAnchor/TACZ
+ */
+
+package xiao.customgun.neoforge.item.attachment;
+
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.api.distmarker.OnlyIn;
+import net.neoforged.neoforge.client.extensions.common.IClientItemExtensions;
+import xiao.customgun.core.item.attachment.AttachmentItem;
+
+import java.util.function.Consumer;
+
+public class NeoAttachmentItem extends AttachmentItem {
+
+    @OnlyIn(Dist.CLIENT)
+    @Override
+    public void initializeClient(Consumer<IClientItemExtensions> consumer) {
+    }
+}

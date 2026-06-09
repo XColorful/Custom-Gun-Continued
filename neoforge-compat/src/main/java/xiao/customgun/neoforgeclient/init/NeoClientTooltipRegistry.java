cb19f0dc5ba78e311487217947f81fcce20f64cf
XiaoColorful <120188147+XColorful@users.noreply.github.com>
2026-06-09 15:49:27 +0800

    Port #17 to neoforge1.21.1

--- neoforge-compat/src/main/java/xiao/customgun/neoforgeclient/init/NeoClientTooltipRegistry.java
+++ neoforge-compat/src/main/java/xiao/customgun/neoforgeclient/init/NeoClientTooltipRegistry.java
@@ -6,12 +6,12 @@ package xiao.customgun.neoforgeclient.init;
 
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.RegisterClientTooltipComponentFactoriesEvent;
 import xiao.customgun.CustomGun;
 import xiao.customgun.client.init.ClientTooltipRegistry;
 
-@Mod.EventBusSubscriber(value = Dist.CLIENT, modid = CustomGun.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+@EventBusSubscriber(value = Dist.CLIENT, modid = CustomGun.MOD_ID)
 public class NeoClientTooltipRegistry {
 
     private static final ClientTooltipRegistry CLIENT_TOOLTIP_REGISTRY = ClientTooltipRegistry.get();

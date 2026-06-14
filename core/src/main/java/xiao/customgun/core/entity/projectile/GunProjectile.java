f997fe0d9214a67ab83c73196c392636717d1496
XiaoColorful <120188147+XColorful@users.noreply.github.com>
2026-06-15 00:15:26 +0800

    deprecated: Add original EntityKineticBullet fields getter

--- core/src/main/java/xiao/customgun/core/entity/projectile/GunProjectile.java
+++ core/src/main/java/xiao/customgun/core/entity/projectile/GunProjectile.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2024-2026 MCModderAnchor (https://github.com/MCModderAnchor)
+ * SPDX-License-Identifier: GPL-3.0-only
+ *
+ * Source: https://github.com/MCModderAnchor/TACZ
+ */
+
 package xiao.customgun.core.entity.projectile;
 
 import net.minecraft.nbt.CompoundTag;
@@ -6,19 +13,31 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import xiao.customgun.core.api.entity.IGunProjectile;
+import xiao.customgun.core.api.entity.gun.GunPropertyCache;
 import xiao.customgun.core.api.entity.projectile.GunProjectileDataAccessor;
+import xiao.customgun.core.api.entity.shooter.ILivingShooterGetter;
 import xiao.customgun.core.api.resource.ResourceApi;
+import xiao.customgun.core.config.AmmoConfig;
+import xiao.customgun.core.config.SyncConfig;
+import xiao.customgun.core.resource.data.data.gun.bullet._ExplosionData;
+import xiao.customgun.core.resource.data.data.gun.bullet.damage._DistanceDamageData;
 import xiao.customgun.core.resource.instance.data.AmmoIndexInstance;
 import xiao.customgun.core.resource.instance.data.GunIndexInstance;
 import xiao.customgun.core.util.NBTUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 枪射物 extends 投掷物
  */
 public class GunProjectile extends Projectile implements IGunProjectile, GunProjectileDataAccessor {
+
+    private Vec3 spawnPos;
 
     // --------Cache--------
     private @Nullable GunIndexInstance gunIndexInstanceCache;
@@ -38,6 +57,9 @@ public class GunProjectile extends Projectile implements IGunProjectile, GunProj
         this.setGunLocation(this, gunLocation);
         this.setGunDisplayLocation(this, gunDisplayLocation);
         this.setAmmoLocation(this, ammoLocation);
+        this.spawnPos = this.position();
+
+        @Nullable GunPropertyCache gunPropertyCache = livingShooter != null ? ILivingShooterGetter.cgc$fromLivingEntity(livingShooter).cgc$getGunPropertyCache() : null;
 
         this.rebuildCache();
     }
@@ -113,5 +135,101 @@ public class GunProjectile extends Projectile implements IGunProjectile, GunProj
     @Override
     public void setAmmoIndexInstanceCache(AmmoIndexInstance cache) {
         this.ammoIndexInstanceCache = cache;
+    }
+
+    // --------Deprecated--------
+    // 原字段的获取方式
+
+    @Deprecated public ResourceLocation ammoId() {
+        return this.getAmmoLocation(this);
+    }
+    @Deprecated public float speed() {
+        return this.getBulletSpeed(this);
+    }
+    @Deprecated public float gravity() {
+        return this.getGravity(this);
+    }
+    @Deprecated public float friction() {
+        return this.getFriction(this);
+    }
+    @Deprecated public List<_DistanceDamageData> damageAmount() {
+        if (this.gunIndexInstanceCache == null) return new ArrayList<>();
+        return this.gunIndexInstanceCache.getGunData().getBulletData().getBulletSkillData().getDamageCalculation();
+    }
+    @Deprecated public float knockback() {
+        if (this.gunIndexInstanceCache == null) return 0;
+        return this.gunIndexInstanceCache.getGunData().getBulletData().getKnockbackStrength();
+    }
+    @Deprecated public float distanceAmount(@Nullable GunPropertyCache gunPropertyCache) {
+        if (gunPropertyCache == null) return 0;
+//        return gunPropertyCache.getCache("effective_range");
+        return 0;
+    }
+    @Deprecated public @Nullable _ExplosionData explosionData() {
+        if (this.gunIndexInstanceCache == null) return null;
+        return this.gunIndexInstanceCache.getGunData().getBulletData().getExplosionData();
+    }
+    @Deprecated public boolean explosion() {
+        _ExplosionData explosionData = explosionData();
+        return explosionData != null && explosionData.getEnableExplode();
+    }
+    @Deprecated public float explosionDamage() {
+        _ExplosionData explosionData = explosionData();
+        return explosionData != null ? (float) (explosionData.getExplodeDamage() * SyncConfig.DAMAGE_BASE_MULTIPLIER.get()) : 0;
+    }
+    @Deprecated public float explosionRadius() {
+        _ExplosionData explosionData = explosionData();
+        return explosionData != null ? explosionData.getExplodeScale() : 0;
+    }
+    @Deprecated public boolean explosionKnockback() {
+        _ExplosionData explosionData = explosionData();
+        return explosionData != null && explosionData.getEnableKnockback();
+    }
+    @Deprecated public boolean explosionDestroyBlock() {
+        _ExplosionData explosionData = explosionData();
+        return explosionData != null && explosionData.getEnableWorldDestruction() && AmmoConfig.EXPLOSIVE_AMMO_DESTROYS_BLOCK.get();
+    }
+    @Deprecated public int explosionDelayCount() {
+        _ExplosionData explosionData = explosionData();
+        return explosionData != null ? (int) (explosionData.getMaxDelaySeconds() * 20) : 0;
+    }
+    @Deprecated public boolean igniteEntity() {
+        if (this.gunIndexInstanceCache == null) return false;
+        return this.gunIndexInstanceCache.getGunData().getBulletData().isFireAspect();
+    }
+    @Deprecated public int igniteEntityTime() {
+        if (this.gunIndexInstanceCache == null) return 0;
+        return this.gunIndexInstanceCache.getGunData().getBulletData().getFireAspectSeconds();
+    }
+    @Deprecated public float damageModifier() {
+        if (this.gunIndexInstanceCache == null) return 0;
+        return 1f / this.gunIndexInstanceCache.getGunData().getBulletData().getBulletAmount();
+    }
+    @Deprecated public int pierce(@Nullable GunPropertyCache gunPropertyCache) {
+        if (gunPropertyCache == null) return 0;
+//        return gunPropertyCache.getCache("pierce");
+        return 0;
+    }
+    @Deprecated public boolean isTracerAmmo() {
+        return this.getIsTracer(this);
+    }
+    @Deprecated public ResourceLocation gunId() {
+        return this.getGunLocation(this);
+    }
+    @Deprecated public ResourceLocation gunDisplayId() {
+        return this.getGunDisplayLocation(this);
+    }
+    @Deprecated public float getArmorIgnore(@Nullable GunPropertyCache gunPropertyCache) {
+        if (this.gunIndexInstanceCache == null) return 0;
+//        return this.gunIndexInstanceCache.getGunData().getBulletData().getBulletSkillData().getArmorIgnorePercent();
+//        return gunPropertyCache.getCache("armor_ignore");
+        return 0;
+    }
+    @Deprecated public float headshot() {
+        if (this.gunIndexInstanceCache == null) return 1;
+        return this.gunIndexInstanceCache.getGunData().getBulletData().getBulletSkillData().getHeadshotMultiplier();
+    }
+    @Deprecated public float shotDamageMultiplier() {
+        return 0; // 源码没有调用过ScriptAPI的setter
     }
 }

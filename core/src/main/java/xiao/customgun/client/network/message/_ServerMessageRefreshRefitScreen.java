cfecf4d44950b159894e77517500492d04377f95
XiaoColorful <120188147+XColorful@users.noreply.github.com>
2026-07-16 22:46:17 +0800

    refresh attachment property on message

--- core/src/main/java/xiao/customgun/client/network/message/_ServerMessageRefreshRefitScreen.java
+++ core/src/main/java/xiao/customgun/client/network/message/_ServerMessageRefreshRefitScreen.java
@@ -10,13 +10,21 @@ package xiao.customgun.client.network.message;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
 import org.jetbrains.annotations.ApiStatus;
+import xiao.customgun.client.gui.GunRefitScreen;
+import xiao.customgun.core.entity.gun.GunPropertyManager;
 
 @ApiStatus.Internal
 public class _ServerMessageRefreshRefitScreen {
 
     public static void updateScreen() {
-        LocalPlayer player = Minecraft.getInstance().player;
-        // TODO GunRefitScreen
-        // TODO AttachmentPropertyManager
+        Minecraft mc = Minecraft.getInstance();
+        LocalPlayer player = mc.player;
+        if (player != null && mc.screen instanceof GunRefitScreen screen) {
+            // 刷新Screen
+            screen.init();
+
+            // 刷新客户端配件数据
+            GunPropertyManager.postChangeEvent(player, player.getMainHandItem());
+        }
     }
 }

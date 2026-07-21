de63d5d9480f1ea452bfcb69170102203b5ac253
XiaoColorful <120188147+XColorful@users.noreply.github.com>
2026-07-22 01:03:10 +0800

    Port #25 to 1.21.1neoforge

--- neoforge-compat/src/main/java/xiao/customgun/neoforgeclient/event/NeoClientPlayerTickEvent.java
+++ neoforge-compat/src/main/java/xiao/customgun/neoforgeclient/event/NeoClientPlayerTickEvent.java
@@ -4,25 +4,24 @@ import net.minecraft.commands.CommandSource;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Player;
-import net.neoforged.neoforge.event.TickEvent;
+import net.neoforged.neoforge.event.tick.PlayerTickEvent;
 import net.neoforged.bus.api.Event;
 import org.jetbrains.annotations.Nullable;
 import xiao.customgun.client.api.event.IClientPlayerTickEvent;
 import xiao.customgun.core.api.common.McLogicalSide;
 import xiao.customgun.core.api.event.EventType;
-import xiao.customgun.neoforge.common.McSideHelper;
 import xiao.customgun.neoforge.event.NeoEvent;
 
 public class NeoClientPlayerTickEvent extends NeoEvent implements IClientPlayerTickEvent {
 
-    protected TickEvent.PlayerTickEvent playerTickEvent;
+    protected PlayerTickEvent.Post playerTickEvent;
 
     public NeoClientPlayerTickEvent(Event event) {
         super(event);
-        if (event instanceof TickEvent.PlayerTickEvent eventIn) {
+        if (event instanceof PlayerTickEvent.Post eventIn) {
             this.playerTickEvent = eventIn;
         } else {
-            throw new RuntimeException("Expected PlayerTickEvent but received: " + event.getClass().getName());
+            throw new RuntimeException("Expected PlayerTickEvent.Post but received: " + event.getClass().getName());
         }
     }
     @Override public EventType getType() {
@@ -31,12 +30,12 @@ public class NeoClientPlayerTickEvent extends NeoEvent implements IClientPlayerT
 
     @Override
     public McLogicalSide getLogicalSide() {
-        return McSideHelper.convert(playerTickEvent.side);
+        return McLogicalSide.CLIENT;
     }
 
     @Override
     public Player getPlayer() {
-        return playerTickEvent.player;
+        return playerTickEvent.getEntity();
     }
 
     @Override
--- neoforge-compat/src/main/java/xiao/customgun/neoforgeclient/event/events/NeoClientPlayerTickEventManager.java
+++ neoforge-compat/src/main/java/xiao/customgun/neoforgeclient/event/events/NeoClientPlayerTickEventManager.java
@@ -3,7 +3,7 @@ package xiao.customgun.neoforgeclient.event.events;
 import net.neoforged.bus.api.Event;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.event.TickEvent;
+import net.neoforged.neoforge.event.tick.PlayerTickEvent;
 import xiao.customgun.core.api.event.EventPriority;
 import xiao.customgun.core.api.event.EventType;
 import xiao.customgun.core.api.event.IEventHandler;
@@ -51,11 +51,8 @@ public class NeoClientPlayerTickEventManager {
             return new NeoClientPlayerTickEvent(event);
         }
 
-        protected void handle(TickEvent.PlayerTickEvent event) {
-            if (event.phase != TickEvent.Phase.END) {
-                return;
-            }
-            if (event.side.isClient()) {
+        protected void handle(PlayerTickEvent.Post event) {
+            if (event.getEntity().level().isClientSide()) {
                 super.onEvent(event);
             }
         }
@@ -64,30 +61,30 @@ public class NeoClientPlayerTickEventManager {
     public static class ClientPlayerTickProxyHighest extends ClientPlayerTickProxy {
         static final ClientPlayerTickProxyHighest INSTANCE = new ClientPlayerTickProxyHighest();
         @SubscribeEvent(priority = net.neoforged.bus.api.EventPriority.HIGHEST, receiveCanceled = true)
-        public void onEvent(TickEvent.PlayerTickEvent e) { handle(e); }
+        public void onEvent(PlayerTickEvent.Post e) { handle(e); }
     }
 
     public static class ClientPlayerTickProxyHigh extends ClientPlayerTickProxy {
         static final ClientPlayerTickProxyHigh INSTANCE = new ClientPlayerTickProxyHigh();
         @SubscribeEvent(priority = net.neoforged.bus.api.EventPriority.HIGH, receiveCanceled = true)
-        public void onEvent(TickEvent.PlayerTickEvent e) { handle(e); }
+        public void onEvent(PlayerTickEvent.Post e) { handle(e); }
     }
 
     public static class ClientPlayerTickProxyNormal extends ClientPlayerTickProxy {
         static final ClientPlayerTickProxyNormal INSTANCE = new ClientPlayerTickProxyNormal();
         @SubscribeEvent(priority = net.neoforged.bus.api.EventPriority.NORMAL, receiveCanceled = true)
-        public void onEvent(TickEvent.PlayerTickEvent e) { handle(e); }
+        public void onEvent(PlayerTickEvent.Post e) { handle(e); }
     }
 
     public static class ClientPlayerTickProxyLow extends ClientPlayerTickProxy {
         static final ClientPlayerTickProxyLow INSTANCE = new ClientPlayerTickProxyLow();
         @SubscribeEvent(priority = net.neoforged.bus.api.EventPriority.LOW, receiveCanceled = true)
-        public void onEvent(TickEvent.PlayerTickEvent e) { handle(e); }
+        public void onEvent(PlayerTickEvent.Post e) { handle(e); }
     }
 
     public static class ClientPlayerTickProxyLowest extends ClientPlayerTickProxy {
         static final ClientPlayerTickProxyLowest INSTANCE = new ClientPlayerTickProxyLowest();
         @SubscribeEvent(priority = net.neoforged.bus.api.EventPriority.LOWEST, receiveCanceled = true)
-        public void onEvent(TickEvent.PlayerTickEvent e) { handle(e); }
+        public void onEvent(PlayerTickEvent.Post e) { handle(e); }
     }
 }

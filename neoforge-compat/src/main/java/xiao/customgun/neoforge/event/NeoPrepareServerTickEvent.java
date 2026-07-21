168a266c480eb8b859badd7490d7252cbf58a753
XiaoColorful <120188147+XColorful@users.noreply.github.com>
2026-07-22 00:35:02 +0800

    Add missing event (neoforge) #25

---
+++ neoforge-compat/src/main/java/xiao/customgun/neoforge/event/NeoPrepareServerTickEvent.java
@@ -0,0 +1,60 @@
+package xiao.customgun.neoforge.event;
+
+import net.minecraft.commands.CommandSource;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.phys.Vec2;
+import net.minecraft.world.phys.Vec3;
+import net.neoforged.bus.api.Event;
+import net.neoforged.neoforge.event.TickEvent;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import xiao.customgun.core.api.event.EventType;
+import xiao.customgun.core.api.event.IServerTickEvent;
+import xiao.customgun.core.api.minecraft.CommandLevel;
+
+public class NeoPrepareServerTickEvent extends NeoEvent implements IServerTickEvent {
+
+    protected TickEvent.ServerTickEvent serverTickEvent;
+
+    public NeoPrepareServerTickEvent(Event event) {
+        super(event);
+        if (event instanceof TickEvent.ServerTickEvent eventIn) {
+            this.serverTickEvent = eventIn;
+        } else {
+            throw new RuntimeException("Expected ServerTickEvent but received: " + event.getClass().getName());
+        }
+    }
+    @Override public EventType getType() {
+        return EventType.PREPARE_SERVER_TICK_EVENT;
+    }
+
+    @Override
+    public MinecraftServer getServer() {
+        return serverTickEvent.getServer();
+    }
+
+    @Override
+    public @NotNull CommandSourceStack createCommandSourceStack(@Nullable CommandSource source) {
+        MinecraftServer server = this.getServer();
+        return new CommandSourceStack(
+                source != null ? source : CommandSource.NULL,
+                Vec3.ZERO,
+                Vec2.ZERO,
+                server.overworld(),
+                CommandLevel.permission(4),
+                this.getTextName(),
+                this.getDisplayName(),
+                server,
+                null
+        );
+    }
+
+    @Override public String getTextName() {
+        return "NeoPrepareServerTickEvent";
+    }
+    @Override public Component getDisplayName() {
+        return Component.literal(getTextName());
+    }
+}

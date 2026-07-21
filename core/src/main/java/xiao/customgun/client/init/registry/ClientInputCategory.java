25fd3392a6611ef22b87d6b77caca897bc40b0e1
XiaoColorful <120188147+XColorful@users.noreply.github.com>
2026-07-22 02:42:18 +0800

    Port #25 to 1.21.10neoforge

--- core/src/main/java/xiao/customgun/client/init/registry/ClientInputCategory.java
+++ core/src/main/java/xiao/customgun/client/init/registry/ClientInputCategory.java
@@ -4,13 +4,11 @@
 
 package xiao.customgun.client.init.registry;
 
+import net.minecraft.client.KeyMapping;
 import xiao.customgun.client.api.minecraft.input.CustomInputCategory;
 
 public class ClientInputCategory {
-    public static final String CONFIG = CustomInputCategory.CONFIG.getRegistryLocation()
-            .getPath();
-    public static final String PLAYER = CustomInputCategory.PLAYER.getRegistryLocation()
-            .getPath();
-    public static final String SHOOTER = CustomInputCategory.SHOOTER.getRegistryLocation()
-            .getPath();
+    public static final KeyMapping.Category CONFIG = new KeyMapping.Category(CustomInputCategory.CONFIG.getRegistryLocation());
+    public static final KeyMapping.Category PLAYER = new KeyMapping.Category(CustomInputCategory.PLAYER.getRegistryLocation());
+    public static final KeyMapping.Category SHOOTER = new KeyMapping.Category(CustomInputCategory.SHOOTER.getRegistryLocation());
 }

557bf8fb425588f3141ab9f3ef28696856df7f20
XiaoColorful <120188147+XColorful@users.noreply.github.com>
2026-07-22 00:05:40 +0800

    Port #25 to 1.20.2neoforge

--- neoforge-compat/src/main/java/xiao/customgun/neoforgeclient/CustomGunNeoforgeClient.java
+++ neoforge-compat/src/main/java/xiao/customgun/neoforgeclient/CustomGunNeoforgeClient.java
@@ -15,18 +15,28 @@
 package xiao.customgun.neoforgeclient;
 
 import xiao.customgun.client.CustomGunClient;
+import xiao.customgun.client.api.input.IKeyMapping;
+import xiao.customgun.client.api.minecraft.access.IClientAccessTransformer;
 import xiao.customgun.core.api.event.IEventRegister;
 import xiao.customgun.neoforgeclient.event.NeoClientEventRegister;
+import xiao.customgun.neoforgeclient.input.NeoKeyMapping;
+import xiao.customgun.neoforgeclient.minecraft.access.NeoClientAccessTransformer;
 
 public class CustomGunNeoforgeClient {
 
     protected static boolean initialized;
     public static IEventRegister eventRegister;
+    public static IKeyMapping.Creator keyMappingCreator;
+    public static IClientAccessTransformer accessTransformer;
 
     public static void init() {
         if (initialized) return;
         CustomGunNeoforgeClient.eventRegister = new NeoClientEventRegister();
-        CustomGunClient.init();
+        CustomGunNeoforgeClient.keyMappingCreator = new NeoKeyMapping.Creator();
+        CustomGunNeoforgeClient.accessTransformer = new NeoClientAccessTransformer();
+
+        CustomGunClient.init(CustomGunNeoforgeClient.keyMappingCreator,
+                CustomGunNeoforgeClient.accessTransformer);
         initialized = true;
     }
 }

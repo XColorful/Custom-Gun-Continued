c7ec39f27ede8104519855d4b9c836a51c434a01
XiaoColorful <120188147+XColorful@users.noreply.github.com>
2026-07-22 12:51:09 +0800

    Port #25 to 26.2neoforge

--- core/src/main/java/xiao/customgun/client/util/ClientGuiUtils.java
+++ core/src/main/java/xiao/customgun/client/util/ClientGuiUtils.java
@@ -8,16 +8,16 @@ import org.jetbrains.annotations.Nullable;
 public class ClientGuiUtils {
 
     public static @Nullable Screen getCurrentScreen(Minecraft minecraft) {
-        return minecraft.screen; // minecraft.gui.screen()
+        return minecraft.gui.screen();
     }
     public static void setCurrentScreen(Minecraft minecraft, Screen screen) {
-        minecraft.setScreen(screen); // minecraft.gui.setScreen(screen)
+        minecraft.gui.setScreen(screen);
     }
 
     public static @Nullable Overlay getOverlay(Minecraft minecraft) {
-        return minecraft.getOverlay(); // minecraft.gui.overlay()
+        return minecraft.gui.overlay();
     }
     public static void setOverlay(Minecraft minecraft, Overlay overlay) {
-        minecraft.setOverlay(overlay); // minecraft.gui.setOverlay(overlay)
+        minecraft.gui.setOverlay(overlay);
     }
 }

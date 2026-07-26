f9fb0e0a92ddc1dd06699f51ccc8efb65f7b994e
XiaoColorful <120188147+XColorful@users.noreply.github.com>
2026-07-27 00:02:28 +0800

    Port #27 to 1.21.6

--- core/src/main/java/xiao/customgun/core/api/entity/projectile/IGunProjectileValueAccess.java
+++ core/src/main/java/xiao/customgun/core/api/entity/projectile/IGunProjectileValueAccess.java
@@ -4,10 +4,15 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.storage.ValueInput;
 import net.minecraft.world.level.storage.ValueOutput;
+import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import xiao.customgun.core.api.resource.ResourceTag;
+import xiao.customgun.core.resource.data.data.gun.bullet._ExplosionData;
+import xiao.customgun.core.resource.data.data.gun.bullet.damage._DistanceDamageData;
+
+import java.util.List;
 
 @ApiStatus.AvailableSince("1.21.6")
 public interface IGunProjectileValueAccess {
@@ -37,6 +42,18 @@ public interface IGunProjectileValueAccess {
 
     // --------IGunProjectileStateAccess--------
 
+    @Nullable Vec3 getShootPos(ValueInput input);
+    void setShootPos(ValueOutput output, Vec3 shootPos);
+
+    @Nullable List<_DistanceDamageData> getDamageCalculation(ValueInput input);
+    void setDamageCalculation(ValueOutput output, List<_DistanceDamageData> damageCalculation);
+
+    float getArmorIgnorePercent(ValueInput input);
+    void setArmorIgnorePercent(ValueOutput output, float armorIgnorePercent);
+
+    float getHeadshotMultiplier(ValueInput input);
+    void setHeadshotMultiplier(ValueOutput output, float headshotMultiplier);
+
     int getLifetimeTicks(ValueInput input);
     void setLifetimeTicks(ValueOutput output, int lifetimeTicks);
 
@@ -58,10 +75,16 @@ public interface IGunProjectileValueAccess {
     boolean getFireAspect(ValueInput input);
     void setFireAspect(ValueOutput output, boolean fireAspect);
 
+    int getFireAspectSeconds(ValueInput input);
+    void setFireAspectSeconds(ValueOutput output, int fireAspectSeconds);
+
     float getKnockbackStrength(ValueInput input);
     void setKnockbackStrength(ValueOutput output, float knockbackStrength);
 
     boolean hasExtraStateTag(ValueInput input);
     @Nullable CompoundTag getExtraStateTag(ValueInput input);
     void setExtraStateTag(ValueOutput output, CompoundTag extraStateTag);
+
+    @Nullable _ExplosionData getExplosionData(ValueInput input);
+    void setExplosionData(ValueOutput output, _ExplosionData explosionData);
 }

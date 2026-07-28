5afe4ef7e4cb89ab3e8416e4b04181e51fb63e42
XiaoColorful <120188147+XColorful@users.noreply.github.com>
2026-07-28 18:24:34 +0800

    Port #28 to 26.2neoforge

--- neoforge-compat/src/main/java/xiao/customgun/neoforgeclient/event/NeoSubmitCustomGeometryEvent.java
+++ neoforge-compat/src/main/java/xiao/customgun/neoforgeclient/event/NeoSubmitCustomGeometryEvent.java
@@ -8,11 +8,12 @@
 package xiao.customgun.neoforgeclient.event;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.renderer.SubmitNodeCollector;
 import net.minecraft.commands.CommandSource;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.phys.Vec3;
-import net.neoforged.bus.api.Event;
+import net.neoforged.neoforge.client.event.SubmitCustomGeometryEvent;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.Nullable;
 import xiao.customgun.client.api.event.ISubmitCustomGeometryEvent;
@@ -22,34 +23,36 @@ import xiao.customgun.neoforge.event.NeoEvent;
 @ApiStatus.AvailableSince("neoforge26.2")
 public class NeoSubmitCustomGeometryEvent extends NeoEvent implements ISubmitCustomGeometryEvent {
 
-    private final Event typedEvent;
+    private final SubmitCustomGeometryEvent typedEvent;
 
-    public NeoSubmitCustomGeometryEvent(Event event) {
+    public NeoSubmitCustomGeometryEvent(SubmitCustomGeometryEvent event) {
         super(event);
         this.typedEvent = event;
     }
-    @Override public EventType getType() {
+
+    @Override
+    public EventType getType() {
         return EventType.SUBMIT_CUSTOM_GEOMETRY_EVENT;
     }
 
     @Override
     public PoseStack getPoseStack() {
-        return null;
+        return this.typedEvent.getPoseStack();
     }
 
     @Override
     public Vec3 getCamera_getPosition() {
-        return null;
+        return this.typedEvent.getLevelRenderState().cameraRenderState.pos;
     }
 
     @Override
     public float getPartialTick() {
-        return 0;
+        return net.minecraft.client.Minecraft.getInstance().getDeltaTracker().getGameTimeDeltaPartialTick(false);
     }
 
     @Override
-    public Object getSubmitNodeCollector() {
-        return null;
+    public SubmitNodeCollector getSubmitNodeCollector() {
+        return this.typedEvent.getSubmitNodeCollector();
     }
 
     @Override
--- neoforge-compat/src/main/java/xiao/customgun/neoforgeclient/event/events/NeoSubmitCustomGeometryEventManager.java
+++ neoforge-compat/src/main/java/xiao/customgun/neoforgeclient/event/events/NeoSubmitCustomGeometryEventManager.java
@@ -8,6 +8,8 @@
 package xiao.customgun.neoforgeclient.event.events;
 
 import net.neoforged.bus.api.Event;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.neoforge.client.event.SubmitCustomGeometryEvent;
 import net.neoforged.neoforge.common.NeoForge;
 import org.jetbrains.annotations.ApiStatus;
 import xiao.customgun.core.api.event.EventPriority;
@@ -43,41 +45,53 @@ public class NeoSubmitCustomGeometryEventManager {
             super(EventType.SUBMIT_CUSTOM_GEOMETRY_EVENT);
         }
 
-        @Override protected void registerToNeo() {
+        @Override
+        protected void registerToNeo() {
             NeoForge.EVENT_BUS.register(this);
         }
-        @Override protected void unregisterToNeo() {
+
+        @Override
+        protected void unregisterToNeo() {
             NeoForge.EVENT_BUS.unregister(this);
         }
-        @Override protected NeoEvent getNeoEventType(Event event) {
-            return new NeoSubmitCustomGeometryEvent(event);
+
+        @Override
+        protected NeoEvent getNeoEventType(Event event) {
+            return new NeoSubmitCustomGeometryEvent((SubmitCustomGeometryEvent) event);
         }
 
-        protected void handle(Event event) { super.onEvent(event); }
+        protected void handle(SubmitCustomGeometryEvent event) {
+            super.onEvent(event);
+        }
     }
 
     public static class SubmitCustomGeometryProxyHighest extends SubmitCustomGeometryProxy {
         static final SubmitCustomGeometryProxyHighest INSTANCE = new SubmitCustomGeometryProxyHighest();
-        public void onEvent(Event e) { handle(e); }
+        @SubscribeEvent(priority = net.neoforged.bus.api.EventPriority.HIGHEST, receiveCanceled = true)
+        public void onEvent(SubmitCustomGeometryEvent e) { handle(e); }
     }
 
     public static class SubmitCustomGeometryProxyHigh extends SubmitCustomGeometryProxy {
         static final SubmitCustomGeometryProxyHigh INSTANCE = new SubmitCustomGeometryProxyHigh();
-        public void onEvent(Event e) { handle(e); }
+        @SubscribeEvent(priority = net.neoforged.bus.api.EventPriority.HIGH, receiveCanceled = true)
+        public void onEvent(SubmitCustomGeometryEvent e) { handle(e); }
     }
 
     public static class SubmitCustomGeometryProxyNormal extends SubmitCustomGeometryProxy {
         static final SubmitCustomGeometryProxyNormal INSTANCE = new SubmitCustomGeometryProxyNormal();
-        public void onEvent(Event e) { handle(e); }
+        @SubscribeEvent(priority = net.neoforged.bus.api.EventPriority.NORMAL, receiveCanceled = true)
+        public void onEvent(SubmitCustomGeometryEvent e) { handle(e); }
     }
 
     public static class SubmitCustomGeometryProxyLow extends SubmitCustomGeometryProxy {
         static final SubmitCustomGeometryProxyLow INSTANCE = new SubmitCustomGeometryProxyLow();
-        public void onEvent(Event e) { handle(e); }
+        @SubscribeEvent(priority = net.neoforged.bus.api.EventPriority.LOW, receiveCanceled = true)
+        public void onEvent(SubmitCustomGeometryEvent e) { handle(e); }
     }
 
     public static class SubmitCustomGeometryProxyLowest extends SubmitCustomGeometryProxy {
         static final SubmitCustomGeometryProxyLowest INSTANCE = new SubmitCustomGeometryProxyLowest();
-        public void onEvent(Event e) { handle(e); }
+        @SubscribeEvent(priority = net.neoforged.bus.api.EventPriority.LOWEST, receiveCanceled = true)
+        public void onEvent(SubmitCustomGeometryEvent e) { handle(e); }
     }
 }

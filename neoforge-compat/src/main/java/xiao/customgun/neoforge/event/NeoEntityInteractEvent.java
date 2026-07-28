9427bce1003a0f53a3882f99a2328a3295167a87
XiaoColorful <120188147+XColorful@users.noreply.github.com>
2026-07-28 18:14:46 +0800

    minor diffs (neoforge)

--- neoforge-compat/src/main/java/xiao/customgun/neoforge/event/NeoEntityInteractEvent.java
+++ neoforge-compat/src/main/java/xiao/customgun/neoforge/event/NeoEntityInteractEvent.java
@@ -25,6 +25,7 @@ import xiao.customgun.core.api.common.McLogicalSide;
 import xiao.customgun.core.api.event.EventType;
 import xiao.customgun.core.api.event.IEntityInteractEvent;
 import xiao.customgun.core.api.minecraft.CommandLevel;
+import xiao.customgun.core.util.Vec3Utils;
 import xiao.customgun.neoforge.common.McSideHelper;
 
 public class NeoEntityInteractEvent extends NeoEvent implements IEntityInteractEvent {
@@ -92,7 +93,7 @@ public class NeoEntityInteractEvent extends NeoEvent implements IEntityInteractE
         Player player = this.getEntity();
         return new CommandSourceStack(
                 source != null ? source : CommandSource.NULL,
-                this.getBlockPos().getCenter(),
+                Vec3Utils.getCenter(this.getBlockPos()),
                 player.getRotationVector(),
                 (ServerLevel) level,
                 CommandLevel.permission(4),
--- neoforge-compat/src/main/java/xiao/customgun/neoforge/event/NeoRightClickItemEvent.java
+++ neoforge-compat/src/main/java/xiao/customgun/neoforge/event/NeoRightClickItemEvent.java
@@ -24,6 +24,7 @@ import xiao.customgun.core.api.common.McLogicalSide;
 import xiao.customgun.core.api.event.EventType;
 import xiao.customgun.core.api.event.IRightClickItemEvent;
 import xiao.customgun.core.api.minecraft.CommandLevel;
+import xiao.customgun.core.util.Vec3Utils;
 import xiao.customgun.neoforge.common.McSideHelper;
 
 public class NeoRightClickItemEvent extends NeoEvent implements IRightClickItemEvent {
@@ -86,7 +87,7 @@ public class NeoRightClickItemEvent extends NeoEvent implements IRightClickItemE
         Player player = this.getEntity();
         return new CommandSourceStack(
                 source != null ? source : CommandSource.NULL,
-                this.getBlockPos().getCenter(),
+                Vec3Utils.getCenter(this.getBlockPos()),
                 player.getRotationVector(),
                 (ServerLevel) level,
                 CommandLevel.permission(4),

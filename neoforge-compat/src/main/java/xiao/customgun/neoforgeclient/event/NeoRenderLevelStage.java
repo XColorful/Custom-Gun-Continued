433c94e0c3c191e7d588df975efe6ca4c96c303e
XiaoColorful <120188147+XColorful@users.noreply.github.com>
2026-07-28 17:30:55 +0800

    Port #28 to 1.21.10neoforge

--- neoforge-compat/src/main/java/xiao/customgun/neoforgeclient/event/NeoRenderLevelStage.java
+++ neoforge-compat/src/main/java/xiao/customgun/neoforgeclient/event/NeoRenderLevelStage.java
@@ -19,7 +19,7 @@ public enum NeoRenderLevelStage {
     //    AFTER_CUTOUT_MIPPED_BLOCKS_BLOCKS(RenderLevelStageEvent.AfterCutoutMippedBlocks.class, RenderLevelStage.AFTER_CUTOUT_MIPPED_BLOCKS_BLOCKS),
 //    AFTER_CUTOUT_BLOCKS(RenderLevelStageEvent.AftetCutoutBlocks.class, RenderLevelStage.AFTER_CUTOUT_BLOCKS),
     AFTER_ENTITIES(RenderLevelStageEvent.AfterEntities.class, RenderLevelStage.AFTER_ENTITIES),
-    AFTER_BLOCK_ENTITIES(RenderLevelStageEvent.AfterBlockEntities.class, RenderLevelStage.AFTER_BLOCK_ENTITIES),
+    //    AFTER_BLOCK_ENTITIES(RenderLevelStageEvent.AfterBlockEntities.class, RenderLevelStage.AFTER_BLOCK_ENTITIES),
     AFTER_TRANSLUCENT_BLOCKS(RenderLevelStageEvent.AfterTranslucentBlocks.class, RenderLevelStage.AFTER_TRANSLUCENT_BLOCKS),
     AFTER_TRIPWIRE_BLOCKS(RenderLevelStageEvent.AfterTripwireBlocks.class, RenderLevelStage.AFTER_TRIPWIRE_BLOCKS),
     AFTER_PARTICLES(RenderLevelStageEvent.AfterParticles.class, RenderLevelStage.AFTER_PARTICLES),

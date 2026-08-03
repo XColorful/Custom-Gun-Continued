2eb26c6f759e156093c445b10287cf0bedeb8890
XiaoColorful <120188147+XColorful@users.noreply.github.com>
2026-08-03 22:36:29 +0800

    Port #29 to 1.21.1neoforge

--- neoforge-compat/src/main/java/xiao/customgun/neoforge/minecraft/NeoRegistry.java
+++ neoforge-compat/src/main/java/xiao/customgun/neoforge/minecraft/NeoRegistry.java
@@ -8,6 +8,7 @@
 package xiao.customgun.neoforge.minecraft;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.core.Holder;
 import net.minecraft.core.Registry;
 import net.minecraft.core.RegistryAccess;
 import net.minecraft.core.particles.ParticleType;
@@ -49,8 +50,8 @@ public class NeoRegistry implements IMcRegistry {
     @Override public @Nullable MobEffect getMobEffect(Identifier rl) {
         return BuiltInRegistries.MOB_EFFECT.getOptional(rl).orElse(null);
     }
-    @Override public @Nullable MobEffect getMobEffect_orHolder(Identifier rl) {
-        return BuiltInRegistries.MOB_EFFECT.get(rl);
+    @Override public @Nullable Holder<MobEffect> getMobEffect_orHolder(Identifier rl) {
+        var reference = BuiltInRegistries.MOB_EFFECT.get(rl).orElse(null); return reference != null ? reference.getDelegate() : null;
     }
     @Override public @Nullable Identifier getMobEffectRl(MobEffect mobEffect) {
         return BuiltInRegistries.MOB_EFFECT.getKey(mobEffect);

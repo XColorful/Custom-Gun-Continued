c12aae713917fa5c13adcabae90bb224cd007f5e
XiaoColorful <120188147+XColorful@users.noreply.github.com>
2026-08-05 17:37:31 +0800

    Port #31 to 1.21.6

--- core/src/main/java/dev/xcolorful/customgun/client/gui/tooltip/attachment/ClientAttachmentTooltip.java
+++ core/src/main/java/dev/xcolorful/customgun/client/gui/tooltip/attachment/ClientAttachmentTooltip.java
@@ -13,14 +13,12 @@ import dev.xcolorful.customgun.core.gui.tooltip.attachment.AttachmentTooltip;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.tooltip.ClientTooltipComponent;
-import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.joml.Matrix4f;
 
 import java.util.List;
 

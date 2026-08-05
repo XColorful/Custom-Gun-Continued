12e901814f5ba3fea256bdf9d42e4bb5219fe3de
XiaoColorful <120188147+XColorful@users.noreply.github.com>
2026-08-05 20:02:24 +0800

    Port #31 to 26.2

--- core/src/main/java/dev/xcolorful/customgun/core/api/event/projectile/ProjectileHitBlockEvent.java
+++ core/src/main/java/dev/xcolorful/customgun/core/api/event/projectile/ProjectileHitBlockEvent.java
@@ -17,7 +17,6 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.Vec2;
-import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 

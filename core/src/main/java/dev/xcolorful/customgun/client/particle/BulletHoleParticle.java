9309399d1d2fba05fcb3a5ac75b30dc3bfef4524
XiaoColorful <120188147+XColorful@users.noreply.github.com>
2026-08-05 17:15:20 +0800

    Port #31 to 1.21.4

--- core/src/main/java/dev/xcolorful/customgun/client/particle/BulletHoleParticle.java
+++ core/src/main/java/dev/xcolorful/customgun/client/particle/BulletHoleParticle.java
@@ -23,7 +23,6 @@ import net.minecraft.client.renderer.texture.TextureAtlas;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.core.BlockPos;
 import net.minecraft.util.Mth;
-import net.minecraft.world.inventory.InventoryMenu;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.AABB;

a0146e01754df3e488247d8ad29a6dbd1ebd333a
XiaoColorful <120188147+XColorful@users.noreply.github.com>
2026-08-11 22:01:50 +0800

    Update BulletHoleParticle.java

--- core/src/main/java/dev/xcolorful/customgun/client/particle/BulletHoleParticle.java
+++ core/src/main/java/dev/xcolorful/customgun/client/particle/BulletHoleParticle.java
@@ -18,7 +18,6 @@ import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.particle.ParticleProvider;
 import net.minecraft.client.particle.ParticleRenderType;
 import net.minecraft.client.particle.SingleQuadParticle;
-import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.state.level.QuadParticleRenderState;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.core.BlockPos;

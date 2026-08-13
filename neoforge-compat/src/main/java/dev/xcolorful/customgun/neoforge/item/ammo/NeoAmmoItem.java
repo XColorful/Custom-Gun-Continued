d76c9f7976a91d9302d7a4c15315e47b6ae0a70e
XiaoColorful <120188147+XColorful@users.noreply.github.com>
2026-08-13 15:17:59 +0800

    Deprecate vanilla BEWLR (neoforge) #32

--- neoforge-compat/src/main/java/dev/xcolorful/customgun/neoforge/item/ammo/NeoAmmoItem.java
+++ neoforge-compat/src/main/java/dev/xcolorful/customgun/neoforge/item/ammo/NeoAmmoItem.java
@@ -7,32 +7,7 @@
 
 package dev.xcolorful.customgun.neoforge.item.ammo;
 
-import dev.xcolorful.customgun.client.renderer.item.AmmoItemRenderer;
 import dev.xcolorful.customgun.core.item.ammo.AmmoItem;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
-import net.neoforged.api.distmarker.Dist;
-import net.neoforged.api.distmarker.OnlyIn;
-import net.neoforged.neoforge.client.extensions.common.IClientItemExtensions;
-
-import java.util.function.Consumer;
 
 public class NeoAmmoItem extends AmmoItem {
-
-    @OnlyIn(Dist.CLIENT)
-    public static void registerClientExtension(Consumer<IClientItemExtensions> consumer) {
-        consumer.accept(new IClientItemExtensions() {
-            AmmoItemRenderer ammoItemRenderer;
-
-            @Override
-            public BlockEntityWithoutLevelRenderer getCustomRenderer() {
-                if (this.ammoItemRenderer == null) {
-                    Minecraft mc = Minecraft.getInstance();
-                    this.ammoItemRenderer = new AmmoItemRenderer(mc.getBlockEntityRenderDispatcher(), mc.getEntityModels());
-                }
-
-                return this.ammoItemRenderer;
-            }
-        });
-    }
 }

7c1cfd625eb5d0dca412cc5b9462e5fca9c06fc0
XiaoColorful <120188147+XColorful@users.noreply.github.com>
2026-08-13 15:17:59 +0800

    Deprecate vanilla BEWLR (neoforge) #32

--- neoforge-compat/src/main/java/dev/xcolorful/customgun/neoforge/item/gun/NeoGunItem.java
+++ neoforge-compat/src/main/java/dev/xcolorful/customgun/neoforge/item/gun/NeoGunItem.java
@@ -7,38 +7,13 @@
 
 package dev.xcolorful.customgun.neoforge.item.gun;
 
-import dev.xcolorful.customgun.client.renderer.item.GunItemRenderer;
 import dev.xcolorful.customgun.core.item.gun.GunItem;
-import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
-import net.neoforged.api.distmarker.Dist;
-import net.neoforged.api.distmarker.OnlyIn;
 import net.neoforged.neoforge.client.extensions.common.IClientItemExtensions;
 
-import java.util.function.Consumer;
-
+/**
+ * 由于已经不需要注入BEWLR，为了{@link IClientItemExtensions}而使用的注册黑魔法已经不需要了
+ * <br>
+ * 但还是保留这个实现，以便浏览演变历史，以及以后可能需要重载平台接口
+ */
 public class NeoGunItem extends GunItem {
-
-    /**
-     * 预留给扩展模组用:
-     * <ul>
-     *     <li>扩展模组core模块只能继承{@link GunItem}而不是{@link NeoGunItem} (除非不想做平台抽象层)</li>
-     *     <li>因而留一个static接口</li>
-     * </ul>
-     * 比如注册<code>MyGunItem extends GunItem</code>并重载final getter/setter来锁定子Manager
-     */
-    @OnlyIn(Dist.CLIENT)
-    public static void registerClientExtension(Consumer<IClientItemExtensions> consumer) {
-        consumer.accept(new IClientItemExtensions() {
-            GunItemRenderer gunItemRenderer;
-
-            @Override
-            public BlockEntityWithoutLevelRenderer getCustomRenderer() {
-                if (this.gunItemRenderer == null) {
-                    this.gunItemRenderer = new GunItemRenderer();
-                }
-
-                return this.gunItemRenderer;
-            }
-        });
-    }
 }

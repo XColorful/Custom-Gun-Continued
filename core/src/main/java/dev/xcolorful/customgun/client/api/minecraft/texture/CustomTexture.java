9368a755a9138f4cc926f03a325c3277e452d8f4
XiaoColorful <120188147+XColorful@users.noreply.github.com>
2026-08-18 20:57:24 +0800

    Align texture directory with vanilla

--- core/src/main/java/dev/xcolorful/customgun/client/api/minecraft/texture/CustomTexture.java
+++ core/src/main/java/dev/xcolorful/customgun/client/api/minecraft/texture/CustomTexture.java
@@ -5,10 +5,10 @@ import net.minecraft.resources.ResourceLocation;
 import org.jetbrains.annotations.NotNull;
 
 public enum CustomTexture {
-    WHITE(String.format("%s:%s", CustomGun.MOD_ID, "texture/white.png")),
-    WHITE_8x8(String.format("%s:%s", CustomGun.MOD_ID, "texture/white_8x8.png")),
+    WHITE(String.format("%s:%s", CustomGun.MOD_ID, "textures/white.png")),
+    WHITE_8x8(String.format("%s:%s", CustomGun.MOD_ID, "textures/white_8x8.png")),
     // entity
-    GUN_PROJECTILE(String.format("%s:%s", CustomGun.MOD_ID, "texture/entity/gun_projectile.png"));
+    GUN_PROJECTILE(String.format("%s:%s", CustomGun.MOD_ID, "textures/entity/gun_projectile.png"));
 
     public final ResourceLocation location;
     CustomTexture(String location) {

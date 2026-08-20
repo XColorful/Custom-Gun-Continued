6d6df54caccbd1ba65080f85aaf1c05345104b34
XiaoColorful <120188147+XColorful@users.noreply.github.com>
2026-08-20 21:32:59 +0800

    Update NoBackgroundScreen.java

--- core/src/main/java/dev/xcolorful/customgun/client/gui/screen/NoBackgroundScreen.java
+++ core/src/main/java/dev/xcolorful/customgun/client/gui/screen/NoBackgroundScreen.java
@@ -1,6 +1,6 @@
 package dev.xcolorful.customgun.client.gui.screen;
 
-import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.GuiGraphicsExtractor;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 import org.jetbrains.annotations.ApiStatus;
@@ -15,39 +15,39 @@ public abstract class NoBackgroundScreen extends Screen {
     // --------移除虚化背景--------
 
     @ApiStatus.AvailableSince("1.20.2")
-    @Override
+    @Deprecated(since = "26.1.x")
     public void renderTransparentBackground(
-            @NotNull GuiGraphics graphics
+            @NotNull GuiGraphicsExtractor graphics
     ) {
     }
     @ApiStatus.AvailableSince("1.21.1")
-    @Override
+    @Deprecated(since = "26.1.x")
     protected void renderBlurredBackground(
-            @NotNull GuiGraphics graphics
+            @NotNull GuiGraphicsExtractor graphics
     ) {
     }
     @ApiStatus.AvailableSince("1.21.1")
-    @Override
+    @Deprecated(since = "26.1.x")
     protected void renderMenuBackground(
-            @NotNull GuiGraphics graphics
+            @NotNull GuiGraphicsExtractor graphics
     ) {
     }
     @ApiStatus.AvailableSince("26.1.x")
-//    @Override
+    @Override
     public void extractTransparentBackground(
-            @NotNull GuiGraphics graphics
+            @NotNull GuiGraphicsExtractor graphics
     ) {
     }
     @ApiStatus.AvailableSince("26.1.x")
-//    @Override
+    @Override
     protected void extractBlurredBackground(
-            @NotNull GuiGraphics graphics
+            @NotNull GuiGraphicsExtractor graphics
     ) {
     }
     @ApiStatus.AvailableSince("26.1.x")
-//    @Override
+    @Override
     protected void extractMenuBackground(
-            @NotNull GuiGraphics graphics
+            @NotNull GuiGraphicsExtractor graphics
     ) {
     }
 }

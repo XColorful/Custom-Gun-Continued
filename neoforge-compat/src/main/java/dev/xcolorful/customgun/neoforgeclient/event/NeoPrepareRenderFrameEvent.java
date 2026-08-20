6d2698dd61f15861198449eb455a0f760685ab9f
XiaoColorful <120188147+XColorful@users.noreply.github.com>
2026-08-20 14:58:29 +0800

    Port #34 to 1.20.2neoforge

--- neoforge-compat/src/main/java/dev/xcolorful/customgun/neoforgeclient/event/NeoPrepareRenderFrameEvent.java
+++ neoforge-compat/src/main/java/dev/xcolorful/customgun/neoforgeclient/event/NeoPrepareRenderFrameEvent.java
@@ -1,6 +1,6 @@
 package dev.xcolorful.customgun.neoforgeclient.event;
 
-import dev.xcolorful.customgun.client.api.event.IRenderFrameEvent;
+import dev.xcolorful.customgun.client.api.event.IPrepareRenderFrameEvent;
 import dev.xcolorful.customgun.core.api.event.EventType;
 import dev.xcolorful.customgun.neoforge.event.NeoEvent;
 import net.minecraft.commands.CommandSource;
@@ -10,7 +10,7 @@ import net.neoforged.bus.api.Event;
 import net.neoforged.neoforge.client.event.RenderFrameEvent;
 import org.jetbrains.annotations.Nullable;
 
-public class NeoPrepareRenderFrameEvent extends NeoEvent implements IRenderFrameEvent {
+public class NeoPrepareRenderFrameEvent extends NeoEvent implements IPrepareRenderFrameEvent {
 
     protected RenderFrameEvent.Pre renderTickEvent;
 
--- neoforge-compat/src/main/java/dev/xcolorful/customgun/neoforgeclient/event/NeoPrepareRenderGuiEvent.java
+++ neoforge-compat/src/main/java/dev/xcolorful/customgun/neoforgeclient/event/NeoPrepareRenderGuiEvent.java
@@ -1,16 +1,9 @@
-/*
- * Copyright (c) 2025-2026 XiaoColorful (https://github.com/XColorful)
- * SPDX-License-Identifier: GPL-3.0-or-later
- *
- * Source: https://github.com/XColorful/BattleRoyale
- */
-
 package dev.xcolorful.customgun.neoforgeclient.event;
 
-import dev.xcolorful.customgun.client.api.event.IRenderGuiEvent;
+import dev.xcolorful.customgun.client.api.event.IPrepareRenderGuiEvent;
 import dev.xcolorful.customgun.core.api.event.EventType;
 import dev.xcolorful.customgun.neoforge.event.NeoEvent;
-import net.minecraft.client.gui.GuiGraphicsExtractor;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.commands.CommandSource;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.network.chat.Component;
@@ -18,25 +11,25 @@ import net.neoforged.bus.api.Event;
 import net.neoforged.neoforge.client.event.RenderGuiEvent;
 import org.jetbrains.annotations.Nullable;
 
-public class NeoRenderGuiEvent extends NeoEvent implements IRenderGuiEvent {
+public class NeoPrepareRenderGuiEvent extends NeoEvent implements IPrepareRenderGuiEvent {
 
-    private final RenderGuiEvent.Post renderGuiEvent;
+    private final RenderGuiEvent.Pre renderGuiEvent;
 
-    public NeoRenderGuiEvent(Event event) {
+    public NeoPrepareRenderGuiEvent(Event event) {
         super(event);
-        if (event instanceof RenderGuiEvent.Post eventIn) {
+        if (event instanceof RenderGuiEvent.Pre eventIn) {
             this.renderGuiEvent = eventIn;
         } else {
-            throw new RuntimeException("Expected RenderGuiEvent.Post but received: " + event.getClass().getName());
+            throw new RuntimeException("Expected RenderGuiEvent.Pre but received: " + event.getClass().getName());
         }
     }
     @Override public EventType getType() {
-        return EventType.RENDER_GUI_EVENT;
+        return EventType.PREPARE_RENDER_GUI_EVENT;
     }
 
     @Override
-    public GuiGraphicsExtractor getGuiGraphics() {
-        return this.renderGuiEvent.getGuiGraphics();
+    public GuiGraphics getGuiGraphics() {
+        return renderGuiEvent.getGuiGraphics();
     }
 
     @Override
@@ -50,7 +43,7 @@ public class NeoRenderGuiEvent extends NeoEvent implements IRenderGuiEvent {
     }
 
     @Override public String getTextName() {
-        return "NeoRenderGuiEvent";
+        return "NeoPrepareRenderGuiEvent";
     }
     @Override public Component getDisplayName() {
         return Component.literal(getTextName());

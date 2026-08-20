f8c211e495ef159ce12ef612f260df54cd625ecc
XiaoColorful <120188147+XColorful@users.noreply.github.com>
2026-08-20 16:41:00 +0800

    Port #34 to 1.21.1forge

--- forge-compat/src/main/java/dev/xcolorful/customgun/forgeclient/event/ForgePrepareRenderGuiEvent.java
+++ forge-compat/src/main/java/dev/xcolorful/customgun/forgeclient/event/ForgePrepareRenderGuiEvent.java
@@ -3,24 +3,24 @@ package dev.xcolorful.customgun.forgeclient.event;
 import dev.xcolorful.customgun.client.api.event.IPrepareRenderGuiEvent;
 import dev.xcolorful.customgun.core.api.event.EventType;
 import dev.xcolorful.customgun.forge.event.ForgeEvent;
+import dev.xcolorful.customgun.forgeclient.compat.forge.event.PrepareRenderGuiEvent;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.commands.CommandSource;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.network.chat.Component;
-import net.minecraftforge.client.event.RenderGuiEvent;
 import net.minecraftforge.eventbus.api.Event;
 import org.jetbrains.annotations.Nullable;
 
 public class ForgePrepareRenderGuiEvent extends ForgeEvent implements IPrepareRenderGuiEvent {
 
-    private final RenderGuiEvent.Pre renderGuiEvent;
+    private final PrepareRenderGuiEvent renderGuiEvent;
 
     public ForgePrepareRenderGuiEvent(Event event) {
         super(event);
-        if (event instanceof RenderGuiEvent.Pre eventIn) {
+        if (event instanceof PrepareRenderGuiEvent eventIn) {
             this.renderGuiEvent = eventIn;
         } else {
-            throw new RuntimeException("Expected RenderGuiEvent.Pre but received: " + event.getClass().getName());
+            throw new RuntimeException("Expected PrepareRenderGuiEvent but received: " + event.getClass().getName());
         }
     }
     @Override public EventType getType() {
@@ -34,7 +34,7 @@ public class ForgePrepareRenderGuiEvent extends ForgeEvent implements IPrepareRe
 
     @Override
     public float getPartialTick() {
-        return renderGuiEvent.getPartialTick();
+        return renderGuiEvent.getPartialTick().getGameTimeDeltaPartialTick(true);
     }
 
     @Override

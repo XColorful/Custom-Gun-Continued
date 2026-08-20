e947e9066eee4a613175ba6f90fceb4e91e54751
XiaoColorful <120188147+XColorful@users.noreply.github.com>
2026-08-20 19:23:30 +0800

    Port #34 to 1.21.4

--- core/src/main/java/dev/xcolorful/customgun/client/gui/tooltip/gun/ClientGunTooltip.java
+++ core/src/main/java/dev/xcolorful/customgun/client/gui/tooltip/gun/ClientGunTooltip.java
@@ -127,7 +127,7 @@ public class ClientGunTooltip implements ClientTooltipComponent {
     // 跨版本适配层
     // 其他类可直接Ctrl CV
 
-    @Override
+//    @Override
     public int getHeight() {
         return this.getHeight(Minecraft.getInstance().font);
     }
@@ -150,6 +150,7 @@ public class ClientGunTooltip implements ClientTooltipComponent {
     @Override
     public void renderImage(@NotNull Font font,
                             int startX, int startY,
+                            int width, int height,
                             @NotNull GuiGraphics guiGraphics) {
         { // 设置缓存
             this.context.imageFont = font;

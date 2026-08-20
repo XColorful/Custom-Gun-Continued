1c3c8d52a6f65a8e6fb3e6f89da8ef9b9f458dba
XiaoColorful <120188147+XColorful@users.noreply.github.com>
2026-08-20 14:58:29 +0800

    Port #34 to 1.20.2neoforge

--- neoforge-compat/src/main/java/dev/xcolorful/customgun/neoforgeclient/event/NeoRenderOverlayEvent.java
+++ neoforge-compat/src/main/java/dev/xcolorful/customgun/neoforgeclient/event/NeoRenderOverlayEvent.java
@@ -1,42 +1,36 @@
-/*
- * Copyright (c) 2025-2026 XiaoColorful (https://github.com/XColorful)
- * SPDX-License-Identifier: GPL-3.0-or-later
- *
- * Source: https://github.com/XColorful/BattleRoyale
- */
-
 package dev.xcolorful.customgun.neoforgeclient.event;
 
-import dev.xcolorful.customgun.client.api.event.IRenderGuiEvent;
+import dev.xcolorful.customgun.client.api.event.IRenderOverlayEvent;
 import dev.xcolorful.customgun.core.api.event.EventType;
 import dev.xcolorful.customgun.neoforge.event.NeoEvent;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.commands.CommandSource;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.neoforged.bus.api.Event;
-import net.neoforged.neoforge.client.event.RenderGuiEvent;
+import net.neoforged.neoforge.client.event.RenderGuiOverlayEvent;
 import org.jetbrains.annotations.Nullable;
 
-public class NeoRenderGuiEvent extends NeoEvent implements IRenderGuiEvent {
+public class NeoRenderOverlayEvent extends NeoEvent implements IRenderOverlayEvent {
 
-    private final RenderGuiEvent.Post renderGuiEvent;
+    private final RenderGuiOverlayEvent.Post renderGuiEvent;
 
-    public NeoRenderGuiEvent(Event event) {
+    public NeoRenderOverlayEvent(Event event) {
         super(event);
-        if (event instanceof RenderGuiEvent.Post eventIn) {
+        if (event instanceof RenderGuiOverlayEvent.Post eventIn) {
             this.renderGuiEvent = eventIn;
         } else {
-            throw new RuntimeException("Expected RenderGuiEvent.Post but received: " + event.getClass().getName());
+            throw new RuntimeException("Expected RenderGuiOverlayEvent.Post but received: " + event.getClass().getName());
         }
     }
     @Override public EventType getType() {
-        return EventType.RENDER_GUI_EVENT;
+        return EventType.RENDER_OVERLAY_EVENT;
     }
 
     @Override
     public GuiGraphics getGuiGraphics() {
-        return this.renderGuiEvent.getGuiGraphics();
+        return renderGuiEvent.getGuiGraphics();
     }
 
     @Override
@@ -45,12 +39,17 @@ public class NeoRenderGuiEvent extends NeoEvent implements IRenderGuiEvent {
     }
 
     @Override
+    public ResourceLocation getRegistryLocation() {
+        return renderGuiEvent.getOverlay().id();
+    }
+
+    @Override
     public @Nullable CommandSourceStack createCommandSourceStack(@Nullable CommandSource source) {
         return null;
     }
 
     @Override public String getTextName() {
-        return "NeoRenderGuiEvent";
+        return "NeoRenderOverlayEvent";
     }
     @Override public Component getDisplayName() {
         return Component.literal(getTextName());

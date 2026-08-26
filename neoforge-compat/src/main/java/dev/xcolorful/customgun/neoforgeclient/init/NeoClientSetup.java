bd7eb2471f0c0f0430723b74e88502562ffd419d
XiaoColorful <120188147+XColorful@users.noreply.github.com>
2026-08-26 10:46:10 +0800

    Port #36 to 1.21.1neoforge

--- neoforge-compat/src/main/java/dev/xcolorful/customgun/neoforgeclient/init/NeoClientSetup.java
+++ neoforge-compat/src/main/java/dev/xcolorful/customgun/neoforgeclient/init/NeoClientSetup.java
@@ -8,10 +8,10 @@ import dev.xcolorful.customgun.CustomGun;
 import dev.xcolorful.customgun.client.init.ClientSetup;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 
-@Mod.EventBusSubscriber(value = Dist.CLIENT, modid = CustomGun.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+@EventBusSubscriber(value = Dist.CLIENT, modid = CustomGun.MOD_ID)
 public class NeoClientSetup {
 
     private static final ClientSetup INSTANCE = ClientSetup.get();

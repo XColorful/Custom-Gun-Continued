2f584ca1fe2d833edf685ab56a6aace76c96150c
XiaoColorful <120188147+XColorful@users.noreply.github.com>
2026-08-27 23:34:42 +0800

    Back-port 1.21.6 pipeline registry to 1.20.1 #36

--- core/src/main/java/dev/xcolorful/customgun/client/api/minecraft/stencil/IStencilOperator.java
+++ core/src/main/java/dev/xcolorful/customgun/client/api/minecraft/stencil/IStencilOperator.java
@@ -1,9 +1,14 @@
 package dev.xcolorful.customgun.client.api.minecraft.stencil;
 
+import dev.xcolorful.customgun.client.api.minecraft.pipeline.PipelineModifier;
 import org.jetbrains.annotations.ApiStatus;
 
 @ApiStatus.AvailableSince("1.21.6")
 public interface IStencilOperator {
 
     void applyStencil(StencilState state);
+
+    void pushPipelineModifier(PipelineModifier modifier);
+
+    void popPipelineModifier();
 }

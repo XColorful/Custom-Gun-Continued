5773793190d9dd9d164ed0317e1593e828d4735e
XiaoColorful <120188147+XColorful@users.noreply.github.com>
2026-08-28 00:51:18 +0800

    Port render to 1.21.6neoforge #36

--- neoforge-compat/src/main/java/dev/xcolorful/customgun/neoforgeclient/init/NeoClientPipelineModifierRegistry.java
+++ neoforge-compat/src/main/java/dev/xcolorful/customgun/neoforgeclient/init/NeoClientPipelineModifierRegistry.java
@@ -1,23 +1,29 @@
 package dev.xcolorful.customgun.neoforgeclient.init;
 
+import dev.xcolorful.customgun.CustomGun;
 import dev.xcolorful.customgun.client.init.ClientPipelineModifierRegistry;
 import dev.xcolorful.customgun.neoforgeclient.minecraft.pipeline.NeoPipelineModifier;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.pipeline.RegisterPipelineModifiersEvent;
 import org.jetbrains.annotations.ApiStatus;
 
 /*
 为了跨版本提前知道1.21.6neoforge的移植方式，添加此类作为占位符
  */
 @ApiStatus.AvailableSince("1.21.6")
-//@EventBusSubscriber(value = Dist.CLIENT, modid = CustomGun.MOD_ID)
+@EventBusSubscriber(value = Dist.CLIENT, modid = CustomGun.MOD_ID)
 public class NeoClientPipelineModifierRegistry {
 
     private static final ClientPipelineModifierRegistry CLIENT_PIPELINE_MODIFIER_REGISTRY = ClientPipelineModifierRegistry.get();
 
-//    @SubscribeEvent
-    public static void onRegisterPipelineModifier(Object event) {
+    @SubscribeEvent
+    public static void onRegisterPipelineModifier(RegisterPipelineModifiersEvent event) {
         CLIENT_PIPELINE_MODIFIER_REGISTRY.onRegisterPipelineModifiers((modifier) -> {
-            NeoPipelineModifier forgePipelineModifier = NeoPipelineModifier.of(modifier);
-            var registryKey = forgePipelineModifier.registryKey;
+            NeoPipelineModifier neoPipelineModifier = NeoPipelineModifier.of(modifier);
+            var registryKey = neoPipelineModifier.registryKey;
+            event.register(registryKey, neoPipelineModifier.modifier);
         });
     }
 }

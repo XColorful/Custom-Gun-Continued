e9bcb179cd0dfdf9c3ef97a569d5415e90c31283
XiaoColorful <120188147+XColorful@users.noreply.github.com>
2026-08-27 14:51:08 +0800

    Back-port 1.21.4 render diffs to 1.20.1 #36

--- forge-compat/src/main/java/dev/xcolorful/customgun/forgeclient/init/ForgeClientSetup.java
+++ forge-compat/src/main/java/dev/xcolorful/customgun/forgeclient/init/ForgeClientSetup.java
@@ -4,20 +4,37 @@
 
 package dev.xcolorful.customgun.forgeclient.init;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import dev.xcolorful.customgun.CustomGun;
 import dev.xcolorful.customgun.client.init.ClientSetup;
+import dev.xcolorful.customgun.client.util.ClientRenderUtils;
+import net.minecraft.client.Minecraft;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import org.jetbrains.annotations.ApiStatus;
 
 @Mod.EventBusSubscriber(value = Dist.CLIENT, modid = CustomGun.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ForgeClientSetup {
 
-    private static final ClientSetup INSTANCE = ClientSetup.get();
+    private static final ClientSetup CLIENT_SETUP = ClientSetup.get();
 
     @SubscribeEvent
     public static void onClientSetup(FMLClientSetupEvent event) {
-        event.enqueueWork(INSTANCE::onClientSetup);
+        event.enqueueWork(CLIENT_SETUP::onClientSetup);
+    }
+
+    /*
+    为了跨版本提前知道1.21.4neoforge的移植方式，添加此类作为占位符
+     */
+    @SubscribeEvent
+    public static void onConfigureMainRenderTarget(FMLCommonSetupEvent event) {
+        CLIENT_SETUP.onConfigureMainRenderTarget(ForgeClientSetup::ensureMainRenderTargetStencil);
+    }
+    @ApiStatus.Internal
+    public static void ensureMainRenderTargetStencil() {
+        RenderSystem.recordRenderCall(() -> ClientRenderUtils.getMainRenderTarget(Minecraft.getInstance()).enableStencil());
     }
 }

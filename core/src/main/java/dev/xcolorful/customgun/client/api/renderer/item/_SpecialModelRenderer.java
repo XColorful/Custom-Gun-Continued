36d97494caab431d0f69bbecd79a0cf3677bb5cc
XiaoColorful <120188147+XColorful@users.noreply.github.com>
2026-08-28 16:23:44 +0800

    Update _SpecialModelRenderer.java

--- core/src/main/java/dev/xcolorful/customgun/client/api/renderer/item/_SpecialModelRenderer.java
+++ core/src/main/java/dev/xcolorful/customgun/client/api/renderer/item/_SpecialModelRenderer.java
@@ -4,7 +4,6 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.serialization.MapCodec;
 import dev.xcolorful.customgun.CustomGun;
 import net.minecraft.client.model.geom.EntityModelSet;
-import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.SubmitNodeCollector;
 import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;

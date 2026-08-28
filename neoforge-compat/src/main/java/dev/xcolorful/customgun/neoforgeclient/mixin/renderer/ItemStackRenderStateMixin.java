dc75fe47ff5a6456fe8dae770058bca2408649c0
XiaoColorful <120188147+XColorful@users.noreply.github.com>
2026-08-28 19:28:13 +0800

    Back-port 26.1.x render to 1.20.2neoforge #36

---
+++ neoforge-compat/src/main/java/dev/xcolorful/customgun/neoforgeclient/mixin/renderer/ItemStackRenderStateMixin.java
@@ -0,0 +1,22 @@
+package dev.xcolorful.customgun.neoforgeclient.mixin.renderer;
+
+import dev.xcolorful.customgun.client.api.renderer.item.ItemDisplayContextTracker;
+import net.minecraft.world.item.ItemDisplayContext;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+/*
+为了跨版本提前知道26.1neoforge的移植方式，添加此类作为占位符
+ */
+//@Mixin(ItemStackRenderState.class)
+public class ItemStackRenderStateMixin {
+
+    ItemDisplayContext displayContext;
+
+    private void cgc$captureDisplayContext(CallbackInfo ci) {
+        ItemDisplayContextTracker.push(this.displayContext);
+    }
+
+    private void cgc$clearDisplayContext(CallbackInfo ci) {
+        ItemDisplayContextTracker.pop();
+    }
+}

b465c81c425c12a0df80ef68ba861a407c466f70
XiaoColorful <120188147+XColorful@users.noreply.github.com>
2026-08-28 19:33:32 +0800

    Update NeoBEWLR.java
    - F*ck IDEA

--- neoforge-compat/src/main/java/dev/xcolorful/customgun/neoforgeclient/renderer/item/NeoBEWLR.java
+++ neoforge-compat/src/main/java/dev/xcolorful/customgun/neoforgeclient/renderer/item/NeoBEWLR.java
@@ -3,6 +3,7 @@ package dev.xcolorful.customgun.neoforgeclient.renderer.item;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.serialization.MapCodec;
 import dev.xcolorful.customgun.client.api.item.IItemBEWLR;
+import dev.xcolorful.customgun.client.api.renderer.item.ItemDisplayContextTracker;
 import dev.xcolorful.customgun.client.api.renderer.item._SpecialModelRenderer;
 import net.minecraft.client.model.geom.EntityModelSet;
 import net.minecraft.client.renderer.MultiBufferSource;
@@ -42,7 +43,7 @@ public class NeoBEWLR implements
                        boolean hasFoilType) {
         if (itemStack == null || !(itemStack.getItem() instanceof IItemBEWLR itemBEWLR)) return;
 
-        ItemDisplayContext itemDisplayContext = dev.xcolorful.customgun.client.api.renderer.item.ItemDisplayContextTracker.current();
+        ItemDisplayContext itemDisplayContext = ItemDisplayContextTracker.current();
         if (true) itemDisplayContext = displayContext; // 26.1移除
 
         var bewlr = itemBEWLR.cgc$getBEWLR(); // 用var就跟ResourceLocation是一样的手法 (把import给隐身，省一个移植修改)，除此之外没别的用意

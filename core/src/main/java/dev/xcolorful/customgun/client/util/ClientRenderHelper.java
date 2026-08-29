1953473b493d58b2b6923e25f560780442f29a23
XiaoColorful <120188147+XColorful@users.noreply.github.com>
2026-08-29 16:39:50 +0800

    Back-port 26.2 render type pipeline back to 1.21.11 #36

--- core/src/main/java/dev/xcolorful/customgun/client/util/ClientRenderHelper.java
+++ core/src/main/java/dev/xcolorful/customgun/client/util/ClientRenderHelper.java
@@ -27,6 +27,7 @@ import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.SubmitNodeCollector;
 import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
 import net.minecraft.client.renderer.entity.player.AvatarRenderer;
+import net.minecraft.client.renderer.rendertype.RenderType;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.world.entity.HumanoidArm;
 import net.minecraft.world.entity.player.PlayerModelPart;

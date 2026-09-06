3fd7ae4c8905e40cbbb1c4c312622ac9a8367fc5
XiaoColorful <120188147+XColorful@users.noreply.github.com>
2026-09-06 15:55:02 +0800

    Update _DefaultGunFire.java

--- core/src/main/java/dev/xcolorful/customgun/core/gun/attack/_DefaultGunFire.java
+++ core/src/main/java/dev/xcolorful/customgun/core/gun/attack/_DefaultGunFire.java
@@ -219,6 +219,7 @@ public class _DefaultGunFire {
                         livingShooter,
                         context.gunLocation, context.gunDisplayLocation, context.ammoLocation);
                 // TODO GunProjectile applyShotgunDamageSpread, setShotDamageMultiplier
+                // 子弹旋转角度和速度
                 iGun.doBulletSpread(iLivingShooter.cgc$getShooterProperty(),
                         iGun, gunItem,
                         iLivingShooter, livingShooter,

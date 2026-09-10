64108a5369949256e3a80089a3d7473a1355daee
XiaoColorful <120188147+XColorful@users.noreply.github.com>
2026-09-10 11:35:52 +0800

    Port #45 to 1.20.2neoforge

--- neoforge-compat/src/main/java/dev/xcolorful/customgun/neoforgeclient/minecraft/access/NeoClientAccessTransformer.java
+++ neoforge-compat/src/main/java/dev/xcolorful/customgun/neoforgeclient/minecraft/access/NeoClientAccessTransformer.java
@@ -3,7 +3,10 @@ package dev.xcolorful.customgun.neoforgeclient.minecraft.access;
 import com.mojang.blaze3d.pipeline.RenderPipeline;
 import dev.xcolorful.customgun.client.api.minecraft.access.IClientAccessTransformer;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.network.chat.Component;
+import org.jetbrains.annotations.Nullable;
 
 public class NeoClientAccessTransformer implements IClientAccessTransformer {
 
@@ -12,6 +15,17 @@ public class NeoClientAccessTransformer implements IClientAccessTransformer {
             Minecraft minecraft
     ) {
         minecraft.startUseItem();
+    }
+
+    @Override public @Nullable Component
+    getOverlayMessageString(
+            Gui gui
+    ) {
+        // [1.20.1, 26.2)
+        return gui.overlayMessageString;
+
+        // [26.2, )
+//        return gui.hud.overlayMessageString;
     }
 
     @Override public Object

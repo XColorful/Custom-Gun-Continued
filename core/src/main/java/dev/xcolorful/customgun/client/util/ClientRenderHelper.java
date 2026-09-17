31b4b5dbc276c7c705f475b4b58653052584d59f
XiaoColorful <120188147+XColorful@users.noreply.github.com>
2026-09-17 14:05:48 +0800

    Update ClientRenderHelper.java

--- core/src/main/java/dev/xcolorful/customgun/client/util/ClientRenderHelper.java
+++ core/src/main/java/dev/xcolorful/customgun/client/util/ClientRenderHelper.java
@@ -23,6 +23,7 @@ import dev.xcolorful.customgun.client.compat.optifine.OptifineCompat;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.client.renderer.SubmitNodeCollector;
 import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
 import net.minecraft.client.renderer.entity.player.AvatarRenderer;

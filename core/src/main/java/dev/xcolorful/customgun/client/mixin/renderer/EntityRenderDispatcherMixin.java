15ded5805b8e33784a55365bbec4e209673e37c9
XiaoColorful <120188147+XColorful@users.noreply.github.com>
2026-09-17 20:19:09 +0800

    Port #51 to 1.21.6

--- core/src/main/java/dev/xcolorful/customgun/client/mixin/renderer/EntityRenderDispatcherMixin.java
+++ core/src/main/java/dev/xcolorful/customgun/client/mixin/renderer/EntityRenderDispatcherMixin.java
@@ -6,18 +6,16 @@ import dev.xcolorful.customgun.client.renderer.entity.EntityHitboxRenderer;
 import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
 import net.minecraft.world.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-// @Deprecated(since = "1.21.6")
+@Deprecated(since = "1.21.6")
 @Mixin(EntityRenderDispatcher.class)
 public class EntityRenderDispatcherMixin {
 
     /**
      * 拦截 F3+B 实体碰撞箱渲染
      */
-    @Inject(method = "renderHitbox", at = @At("HEAD"), cancellable = true)
+//    @Inject(method = "renderHitbox", at = @At("HEAD"), cancellable = true)
     private static void cgc$hideEntityHitbox(PoseStack poseStack,
                                              VertexConsumer vertexConsumer,
                                              Entity entity,

35f6257e4e9e823d0ea58c96a5efdf9cb5a796a6
XiaoColorful <120188147+XColorful@users.noreply.github.com>
2026-09-18 23:23:27 +0800

    Remove inappropriate GPL-3.0 headers from vanilla contents (neoforge) #53

--- neoforge-compat/src/main/java/dev/xcolorful/customgun/neoforgeclient/event/events/NeoItemTooltipEventManager.java
+++ neoforge-compat/src/main/java/dev/xcolorful/customgun/neoforgeclient/event/events/NeoItemTooltipEventManager.java
@@ -1,10 +1,3 @@
-/*
- * Copyright (c) 2025-2026 XiaoColorful (https://github.com/XColorful)
- * SPDX-License-Identifier: GPL-3.0-or-later
- *
- * Source: https://github.com/XColorful/BattleRoyale
- */
-
 package dev.xcolorful.customgun.neoforgeclient.event.events;
 
 import dev.xcolorful.customgun.core.api.event.EventPriority;

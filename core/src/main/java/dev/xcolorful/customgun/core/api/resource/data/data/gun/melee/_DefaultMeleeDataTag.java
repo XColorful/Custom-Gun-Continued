c277e6ff7fd26a6e42f7e34e014bd52e30f49d5a
XiaoColorful <120188147+XColorful@users.noreply.github.com>
2026-09-18 23:51:37 +0800

    Remove GPL-3.0 headers from data format definitions

--- core/src/main/java/dev/xcolorful/customgun/core/api/resource/data/data/gun/melee/_DefaultMeleeDataTag.java
+++ core/src/main/java/dev/xcolorful/customgun/core/api/resource/data/data/gun/melee/_DefaultMeleeDataTag.java
@@ -1,10 +1,3 @@
-/*
- * Copyright (c) 2024-2026 MCModderAnchor (https://github.com/MCModderAnchor)
- * SPDX-License-Identifier: GPL-3.0-only
- *
- * Source: https://github.com/MCModderAnchor/TACZ
- */
-
 package dev.xcolorful.customgun.core.api.resource.data.data.gun.melee;
 
 import dev.xcolorful.customgun.core.api.resource.data.data.attachment._MeleeModifierDataTag;
